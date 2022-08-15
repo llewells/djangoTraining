3c6dd41788f6d17489f993c3f9b1e6a42bd0b9c7
Simon Llewellyn <48479856+simon-vq@users.noreply.github.com>
2022-08-15 12:40:55 +0000

    Display ony the logged user data.

--- notes/views.py
+++ notes/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import Http404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.views.generic.edit import  DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Notes
 from.forms import  NotesForm
@@ -30,10 +31,14 @@ class NotesDeleteView(DeleteView):
     template_name = 'notes/notes_delete.html'
 
 
-class NotesListView(ListView):
+class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
     context_object_name = "notes"
     template_name = "notes/notes_list.html"
+    login_url = "/admin"
+
+    def get_queryset(self):
+        return sel.request.user.notes.all()
 
 
 class NotesDetailView(DetailView):
