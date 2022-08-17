1e0d4528a28ef7c4143e0c0c3b1e11ed09c09a5c
Simon Llewellyn <48479856+simon-vq@users.noreply.github.com>
2022-08-17 11:31:41 +0000

    added signup page

--- home/views.py
+++ home/views.py
@@ -5,20 +5,24 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+
+from django.shortcuts import redirect
 
 
-# Function BAsed views
-# def home(request):
-#     return render(request, 'home/welcome.html', {'today':datetime.today()})
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'home/register.html'
+    success_url = '/smart/notes'
 
-# @login_required(login_url='/admin')
-# def authorized(request):
-#     return render(request, 'home/authorized.html', {})
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('notes.list')
+        return super().get(request, *args, **kwargs)
 
 
-# Class Based Views
 class HomeView(TemplateView):
-    '''class based view'''
     template_name = 'home/welcome.html'
     extra_context = {'today': datetime.today()}
 
